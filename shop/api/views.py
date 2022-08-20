a135b077ab096adabac8dc1eba0942ac785a56fa
muqsitamir <islammuqsit7@gmail.com>
2022-08-20 07:24:42 +0500

    Replaced Mixins with Generics (DRF)

--- shop/api/views.py
+++ shop/api/views.py
@@ -4,29 +4,12 @@ from shop.api.serializers import ProductSerializer
 from shop.models import Product
 
 
-class ProductViewSet(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.DestroyModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin):
+class ProductViewSet(generics.ListCreateAPIView, generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
-
-    def get_queryset(self):
-        page, limit = int(self.request.GET.get('page', '0')), self.request.GET.get('limit', 12)
-        return Product.objects.all()
 
     def get(self, request, pk=None):
         if not pk:
             return self.list(request)
         return self.retrieve(request, pk)
-
-    def post(self, request):
-        return self.create(request)
-
-    def patch(self, request, pk):
-        return self.update(request, pk,partial=True)
-
-    def delete(self, request, pk):
-        return self.destroy(request, pk)
-
-    def put(self, request, pk):
-        return self.update(request, pk)
-
 
