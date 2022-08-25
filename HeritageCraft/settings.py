54452db2d4aea93177904cc3409b7708ddb31cc6
muqsitamir <islammuqsit7@gmail.com>
2022-08-26 03:12:21 +0500

    "ModelViewSet Class and pagination settings applied"

--- HeritageCraft/settings.py
+++ HeritageCraft/settings.py
@@ -92,7 +92,7 @@ REST_FRAMEWORK = {
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         'rest_framework.permissions.AllowAny',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
 }
 
