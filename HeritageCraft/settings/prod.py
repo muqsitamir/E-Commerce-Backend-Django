bfbee5be605bba4ab64be97112e01756321bbbca
muqsitamir <islammuqsit7@gmail.com>
2022-08-26 11:28:12 +0500

    Prod changed

--- HeritageCraft/settings/prod.py
+++ HeritageCraft/settings/prod.py
@@ -78,9 +78,11 @@ WSGI_APPLICATION = 'HeritageCraft.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/etc/mysql/my.cnf',
-        },
+        'NAME': 'HeritageCraft',
+        'USER': 'djangouser',
+        'PASSWORD': 'password',
+        'HOST': '50.116.7.254',
+        'PORT': '3306',
     }
 }
 
