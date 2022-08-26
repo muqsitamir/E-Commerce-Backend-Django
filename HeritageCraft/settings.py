2c2fb574929318bf311bb648649d9e72f0705f90
muqsitamir <islammuqsit7@gmail.com>
2022-08-26 07:35:36 +0500

    Changed static root setting

--- HeritageCraft/settings.py
+++ HeritageCraft/settings.py
@@ -137,8 +137,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
