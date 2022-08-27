299188ea8a35aeaa6b2cddf40eb9d51dfdc9c64d
muqsitamir <islammuqsit7@gmail.com>
2022-08-27 05:56:23 +0500

    urls static set

--- HeritageCraft/settings/prod.py
+++ HeritageCraft/settings/prod.py
@@ -135,13 +135,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'assets/dist'),
-    os.path.join(BASE_DIR, 'assets/src/css'),
-    os.path.join(BASE_DIR, 'assets/src/scripts'),
-    os.path.join(BASE_DIR, 'assets/src/images'),
-)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
