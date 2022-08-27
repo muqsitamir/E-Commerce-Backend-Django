9479c7a986cb670f77221489a4d724dcef155d31
muqsitamir <islammuqsit7@gmail.com>
2022-08-27 05:13:11 +0500

    Prod changed

--- HeritageCraft/urls.py
+++ HeritageCraft/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('shop/', include("shop.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
